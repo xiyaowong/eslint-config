c8526b41246b7413d3c78ed7981ca56e40feaf81
wongxy <xiyao.wong@foxmail.com>
2023-07-29 20:03:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "rules": {
+    "no-console": "off",
+    "no-multiple-empty-lines": "warn",
+    "@typescript-eslint/prefer-ts-expect-error": "off",
+    "vue/component-tags-order": ["warn", { "order": ["script", "template", "style"] }],
+    "semi": "off",
+    "@typescript-eslint/semi": ["warn", "never"],
+    "quotes": "off",
+    "@typescript-eslint/quotes": ["warn", "single"]
+  }
+}
